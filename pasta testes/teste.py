23848f4f84cda130b9453d63eb5d29a9cc16f1db
Rubens Lima <101185550+rubdelima@users.noreply.github.com>
2022-05-02 16:12:41 -0300

    Update teste.py

--- pasta testes/teste.py
+++ pasta testes/teste.py
@@ -2,15 +2,16 @@ import pygame
 import random
 pygame.init()
 
-#coordenadas da bolona
-x = 400
-y = 300
-tam_bolona = 40
+caimento = 0
 
+#coordenadas da bolona verde
+player_x = 400
+player_y = 300
+tam_bolona = 40
 velocity = 25
-#condição da bolinha
-xa= 100
-ya= 100
+#condição da bolinha vermelha
+bolinha_x = 100
+bolinha_y = 100
 tam_bolinha = 10
 
 janela = pygame.display.set_mode((800,600)) #Criar a janela
@@ -27,30 +28,38 @@ while janela_aberta:
     #comandos para movimentar a bolona
     comandos = pygame.key.get_pressed()
     if comandos[pygame.K_UP]:
-        y -= velocity
+        player_y -= velocity
     if comandos[pygame.K_DOWN]:
-        y += velocity
+        player_y += velocity
     if comandos[pygame.K_LEFT]:
-        x -= velocity
+        player_x -= velocity
     if comandos[pygame.K_RIGHT]:
-        x += velocity
-    if comandos[pygame.K_SPACE]:
-        tam_bolona += 5
+        player_x += velocity
+    # if comandos[pygame.K_SPACE]:
+    #     tam_bolona += 5
     
-    #atualizar e não deixar um "rastro"
+    #atualizar e não deibolinha_x r um "rastro"
     janela.fill((0,0,0))
     
     #nossa bolona
-    pygame.draw.circle(janela, (0,255,0), (x,y), tam_bolona) #o primeiro é o rgb (vermelho, verde, azul), o segundo é a posição e o último é o raio
+    pygame.draw.circle(janela, (0,255,0), (player_x, player_y), tam_bolona) #o primeiro é o rgb (vermelho, verde, azul), o segundo é a posição e o último é o raio
     #bolinhas para pegar
-    pygame.draw.circle(janela, (255,0,0), (xa,ya), tam_bolinha) #o primeiro é o rgb (vermelho, verde, azul), o segundo é a posição e o último é o raio
+    pygame.draw.circle(janela, (255,0,0), (bolinha_x ,bolinha_y ), tam_bolinha) #o primeiro é o rgb (vermelho, verde, azul), o segundo é a posição e o último é o raio
     
-    if (xa+tam_bolinha - x+tam_bolona == 0) or (ya+tam_bolinha - y+tam_bolona == 0):
-        tam_bolona += tam_bolinha
+    if ((bolinha_x + tam_bolinha) - (player_x + tam_bolona)) >= 0 and ((bolinha_y + tam_bolinha) - (player_y + tam_bolona) >= 0):
         random.seed();
-        xa = random.randint(100,700)
-        ya = random.randint(100,500)
+        tam_bolona += 10
+        bolinha_x  = random.randint(100,700)
+        bolinha_y  = 0
         tam_bolinha = random.randint(10,40)
+
+    
+    #movimentacao bolinha
+    caimento += 1
+    if caimento == 10:
+        bolinha_y  += 10
+        caimento = 0
+    
         
     
     pygame.display.update()
