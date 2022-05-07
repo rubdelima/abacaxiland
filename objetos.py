eac3d8a22e8baf6603365b3510c5e8988988cd4c
Nathaniel <57302128+NathanRocha01@users.noreply.github.com>
2022-05-06 22:13:48 -0300

    Update objetos.py
    Adicionando dificuldade

--- objetos.py
+++ objetos.py
@@ -4,73 +4,21 @@ from player import *
 
 class Objetos():
 
-    def __init__(self, tamanho, valor, coletados, img, pos_x=randint(0,836), pos_y=0):
+    def __init__(self, tamanho, valor, img, pos_x=randint(0,836), pos_y=0, aux = 0):
         self.tamanho = tamanho
         self.valor = valor
-        self.coletados = coletados
         self.pos_x = pos_x 
         self.pos_y = pos_y 
         self.img = pygame.image.load(img).convert_alpha()
 
-    def coletar(self, janela):
-        pass
+    def cair(self, janela, velocidade):
+        self.pos_y += velocidade
+        janela.blit(self.img, (self.pos_x, self.pos_y))
 
-
-class Spawn(Objetos):
-    def __init__(self, interval, aux=0):
-        self.aux = aux
-        self.interval = interval
-        
-    def aparecer(self, janela, current_time, player_x, player_y):
-        self.player_x = player_x
-        self.player_y = player_y
-        self.aux += 1
-        self.current_time = current_time
-        if self.aux > 1:   
-            print(self.current_time - self.aux1)
-        self.aux1 = self.current_time
-        
-        if self.aux == 1:
-            self.interval = 0
-            self.tipo = randint(0, 3)
-            if self.tipo == 0:
-                self.fruta =  Objetos(64, 10, 0, 'images/morango.png', randint(0, 836))
-            elif self.tipo == 1:
-                self.fruta = Objetos(64, 5, 0, 'images/abacaxi.png', randint(0, 836))
-            elif self.tipo == 2:
-                self.fruta = Objetos(64, 7, 0, 'images/pitanga.png', randint(0, 836))
-            elif self.tipo == 3:
-                self.fruta = Objetos(64, 9, 0, 'images/banana.png', randint(0, 836))
-
-        
-        self.janela = janela
-
-        # Aqui é onde a fruta é mostrada na tela
-        if self.current_time - self.interval > 2000:
-            self.fruta.pos_y += 1
-            self.janela.blit(self.fruta.img, (self.fruta.pos_x, self.fruta.pos_y))
-            if self.fruta.pos_y >= 568:
-                self.aux = 0
-            elif  self.player_y - 32 <= self.fruta.pos_y <= self.player_y + 32 and self.player_x - 32 <= self.fruta.pos_x <= self.player_x + 32:
-                self.aux = 0    
-                self.interval = current_time
-                print(self.interval)
-
-    def colisao(self):
-        colidiu = False
-        if self.player_y - 32 <= self.fruta.pos_y <= self.player_y + 32 and self.player_x - 32 <= self.fruta.pos_x <= self.player_x + 32:
-            colidiu = True
-            self.aux = 0
-        return colidiu
-    def aparecer_bomba(self):
-        pass
-
-    def aparecer_nuclear(self):
-        pass
-    
-
-        
-
-
-
-
+    def colisao(self, player_x, player_y):
+        valor = -1;
+        if player_y - 32 <= self.pos_y <= player_y + 32 and player_x - 32 <= self.pos_x <= player_x + 32:
+            valor = self.valor;
+        if self.pos_y >= 568:
+            valor = 0;
+        return valor
