7166a4845ec6fd5e8577227344bb865ed6aad451
Iasmym Mendes <ilmf@cin.ufpe.br>
2022-05-09 01:30:15 -0300

    Criei a classe Botao e adicionei um menu interativo com botões clicaveis e o título do jogo. Criei a pasta fonte para armazenar a fonte do título e adicionei o efeito sonoro do clique do botão start a pasta music

--- Main.py
+++ Main.py
@@ -17,6 +17,10 @@ pygame.display.set_caption("Abacaxiland") # Criando o nome da screen
 icon = pygame.image.load('images/logo.png').convert_alpha()
 pygame.display.set_icon(icon)
 
+# Imagens dos botões
+botao_start_img = pygame.image.load('images/start128_verde.png').convert_alpha()
+botao_exit_img = pygame.image.load('images/exit128_verde.png').convert_alpha()
+
 # Musica de background
 pygame.mixer.init()
 pygame.mixer.music.load('music/background_music.wav')
@@ -36,6 +40,47 @@ banana = Pontuacao_fruta('images/banana.png',(255,255,0), 0, 20, 164)
 # nuclear = Pontuacao_fruta('images/nuclear-bomb.png', randint(0, 644))
 # variável pra estabelecer o intervalo de tempo
 
+# Criando menu
+menu = True
+fonte = pygame.font.Font('fonte/Fresh Fruit.ttf', 70)
+titulo = fonte.render(f'ABACAXILAND', True, (0, 204, 0))
+
+# Criando os botões do menu
+class Botao:
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.clique = False
+        
+    def desenhar_botao(self):
+        acao = False
+        
+        # Pegar posição do mouse
+        pos = pygame.mouse.get_pos()
+        
+        # Checar ações do mouse
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clique == False:
+                acao = True
+                self.clique = True
+                # Efeito sonoro de clicar no botão de start
+                musica_coleta = pygame.mixer.Sound('music/menu_clique.wav')
+                musica_coleta.play()
+        
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clique = False
+            
+        # Desenhar botão
+        screen.blit(self.image, self.rect)
+        
+        return acao
+
+# Criar botões
+botao_start = Botao(370, 300, botao_start_img)
+botao_exit = Botao(370, 400, botao_exit_img) 
+
 aux = 0
 caindo = [];
 nivel = 1;
@@ -49,53 +94,62 @@ while running:
     # screen do game
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
-   
-    # Movendo o personagem
-    comando = pygame.key.get_pressed()
-    player1.mover(comando)
-
-    # desenho a posição do player na tela
-    player1.desenhar_player(screen, 'images/player1.png')
     
-    # ajeitando o tempo para o spawn
-    current_time = pygame.time.get_ticks()
-    if aux == 1:
-        interval = current_time
+    if menu == True:
+        # Nome do jogo
+        screen.blit(titulo, (235, 150))
+        
+        if botao_exit.desenhar_botao():
+            running = False
+        if botao_start.desenhar_botao():
+            menu = False
+    else:
+        # Movendo o personagem
+        comando = pygame.key.get_pressed()
+        player1.mover(comando)
 
-    #pegando a diferença de objetos mostrado para serem adicionados de acordo com o nivel
-    diff = abs(len(caindo) - nivel);
-    if(diff != 0):
-        objetos = [Objetos(64, 10, 'images/morango.png', randint(0, 836)),Objetos(64, 5, 'images/abacaxi.png', randint(0, 836)),Objetos(64, 7, 'images/pitanga.png', randint(0, 836)),Objetos(64, 9, 'images/banana.png', randint(0, 836))]
-        for i in range(diff):
-            caindo.append(objetos[randint(0,3)]);
-    if(len(caindo) > 0):
-        for obj in caindo:
-            obj.cair(screen,nivel);
-            valor = obj.colisao(player1.pos_x, player1.pos_y);
-            if(valor >= 0):
-                if valor == 10:
-                    morango.ponto += 1;
-                elif valor == 5:
-                    abacaxi.ponto += 1;
-                elif valor == 7:
-                    pitanga.ponto += 1;
-                elif valor == 9:
-                    banana.ponto += 1;
-                caindo.remove(obj);
+        # desenho a posição do player na tela
+        player1.desenhar_player(screen, 'images/player1.png')
+        
+        # ajeitando o tempo para o spawn
+        current_time = pygame.time.get_ticks()
+        if aux == 1:
+            interval = current_time
 
-    total = (abacaxi.ponto*5) + (pitanga.ponto*7) + (morango.ponto*10) + (banana.ponto*9);
-    if(total >= 100 and total < 200):
-        nivel = 2;
-    elif(total >= 200):
-        nivel = 3;
-    pontuacao_total = Total(total)
-    pontuacao_total.mostrar_total(screen)
-    # área da pontuação
-    
-    abacaxi.mostrar_pontos(screen)
-    pitanga.mostrar_pontos(screen)
-    morango.mostrar_pontos(screen)
-    banana.mostrar_pontos(screen)
+        #pegando a diferença de objetos mostrado para serem adicionados de acordo com o nivel
+        diff = abs(len(caindo) - nivel);
+        if(diff != 0):
+            objetos = [Objetos(64, 10, 'images/morango.png', randint(0, 836)),Objetos(64, 5, 'images/abacaxi.png', randint(0, 836)),Objetos(64, 7, 'images/pitanga.png', randint(0, 836)),Objetos(64, 9, 'images/banana.png', randint(0, 836))]
+            for i in range(diff):
+                caindo.append(objetos[randint(0,3)]);
+        if(len(caindo) > 0):
+            for obj in caindo:
+                obj.cair(screen,nivel);
+                valor = obj.colisao(player1.pos_x, player1.pos_y);
+                if(valor >= 0):
+                    if valor == 10:
+                        morango.ponto += 1;
+                    elif valor == 5:
+                        abacaxi.ponto += 1;
+                    elif valor == 7:
+                        pitanga.ponto += 1;
+                    elif valor == 9:
+                        banana.ponto += 1;
+                    caindo.remove(obj);
+
+        total = (abacaxi.ponto*5) + (pitanga.ponto*7) + (morango.ponto*10) + (banana.ponto*9);
+        if(total >= 100 and total < 200):
+            nivel = 2;
+        elif(total >= 200):
+            nivel = 3;
+        pontuacao_total = Total(total)
+        pontuacao_total.mostrar_total(screen)
+        # área da pontuação
+        
+        abacaxi.mostrar_pontos(screen)
+        pitanga.mostrar_pontos(screen)
+        morango.mostrar_pontos(screen)
+        banana.mostrar_pontos(screen)
     
 
     
